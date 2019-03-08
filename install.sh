6d0a05f7816e7f930daf190a8af799b363c59297
music_brain88 <music.brain88@gmail.com>
2019-03-08 19:17:24 +0900

    relink

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ set -u
 
 echo "start setup..."
 
-ln -snfv ~/dotfiles/nvim/dein.toml ~/.config/nvim/dein.toml
-ln -snfv ~/dotfiles/nvim/init.d ~/.config/nvim/init.d
-ln -snfv ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -snfv ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -snfv ~/mac_dotfiles/nvim/dein.toml ~/.config/nvim/dein.toml
+ln -snfv ~/mac_dotfiles/nvim/init.d ~/.config/nvim/init.d
+ln -snfv ~/mac_dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -snfv ~/mac_dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
 echo "finish setup"
 echo "next you call dein script"
